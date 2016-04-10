f6c5eb606f8f6a7cce1e28b821455cc8f6e5bc5c
Daniel Li <neolee11@yahoo.com>
2016-04-09 23:58:19 -0400

    check in

--- RecipeTraderDotNet.Core/Domain/Market/UserReview.cs
+++
@@ -1,8 +0,0 @@
-namespace RecipeTraderDotNet.Core.Domain.Market
-{
-    public class UserReview
-    {
-        public double Rating { get; set; }
-        public string Comment { get; set; }
-    }
-}
--- RecipeTraderDotNet.Data/Repositories/Memory/PublicRecipeRepository.cs
+++ RecipeTraderDotNet.Data/Repositories/Memory/PublicRecipeRepository.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RecipeTraderDotNet.Core.Domain.Recipe;
+using RecipeTraderDotNet.Core.Domain.Repositories;
 
 namespace RecipeTraderDotNet.Data.Repositories.Memory
 {
-    class PublicRecipeRepository
+    public class PublicRecipeRepository : IPublicRecipeRepository
     {
+        public List<PublicRecipe> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PublicRecipe GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(PublicRecipe t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(PublicRecipe t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PublicRecipe GetByUserIdAndTitle(string userId, string title)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
