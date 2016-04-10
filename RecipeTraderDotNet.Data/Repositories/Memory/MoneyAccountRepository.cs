f292788712e6d3a76abf1a3d85b4e518e3d5ae44
Daniel Li <neolee11@yahoo.com>
2016-04-09 23:57:26 -0400

    progress

---
+++ RecipeTraderDotNet.Data/Repositories/Memory/MoneyAccountRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipeTraderDotNet.Core.Domain.Repositories;
+using RecipeTraderDotNet.Core.Domain.User;
+
+namespace RecipeTraderDotNet.Data.Repositories.Memory
+{
+    public class MoneyAccountRepository : IMoneyAccountRepository
+    {
+        public List<MoneyAccount> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MoneyAccount GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(MoneyAccount t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(MoneyAccount t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MoneyAccount GetUserMoneyAccount(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecipeTraderDotNet.Data/Repositories/Memory/PrivateRecipeRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipeTraderDotNet.Core.Domain.Recipe;
+using RecipeTraderDotNet.Core.Domain.Repositories;
+
+namespace RecipeTraderDotNet.Data.Repositories.Memory
+{
+    public class PrivateRecipeRepository : IPrivateRecipeRepository
+    {
+        public List<PrivateRecipe> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrivateRecipe GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(PrivateRecipe t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(PrivateRecipe t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PrivateRecipe> GetUserRecipes(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+//public async Task<PrivateRecipe> GetByIdAsync(int id)
+//{
+//    using (var client = new HttpClient())
+//    {
+//        var baseUrl = "http://localhost:5849/";
+//        client.BaseAddress = new Uri(baseUrl);
+//        client.DefaultRequestHeaders.Accept.Clear();
+//        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+//        var apiUrl = "api/privateRecipe/2";
+//        HttpResponseMessage response = await client.GetAsync(apiUrl);
+//        if (response.IsSuccessStatusCode)
+//        {
+//            var json = await response.Content.ReadAsStringAsync();
+//            var privateRecipe = JsonConvert.DeserializeObject<PrivateRecipe>(json);
+//            return privateRecipe;
+//        }
+
+//        return null;
+//    }
+//}
